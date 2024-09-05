57bd3e0209422bb8409c7b8f3d5a1cba224a758c
Rafael Santos <rafaels.rs011@gmail.com>
2024-09-04 22:05:48 -0300

    atualização

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {
+        titulo:  "Rayssa Leal",
+        descricao: "Rayssa Leal é uma skatista brasileira, conhecida como a 'Fada do Skate'. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil, conquistando a prata nos Jogos Olímpicos de Tóquio 2020",
+        link:"https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma das maiores ginastas da história do Brasil. Com um talento inegável e uma força de vontade admirável, ela conquistou o coração dos brasileiros e se tornou um ícone do esporte nacional.",
+        link:"https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+    {
+        titulo:  'Betriz Souza',
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024. Sua vitória histórica colocou o Brasil no mapa do judô mundial.",
+        link:"https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    }
+   
+]
