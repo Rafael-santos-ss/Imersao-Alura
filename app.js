dd6f6d3d64f73e5e65100525863c3679ab60c7e5
Rafael Santos <rafaels.rs011@gmail.com>
2024-09-06 17:46:58 -0300

    finalização

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log(dados[2]);
+function search(){
+    let section = document.getElementById("resultados-pesquisa");
+    
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if (campoPesquisa == "") {
+        section.innerHTML = "Nada foi encontrado. Você precisa digitar o nome de um atleta ou esporte"
+        return
+    }
+
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+    
+    for (let  dado of dados){
+        
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+          
+            resultados += `<div class="item-resultado">
+                        <h2>
+                            <a href="#" target="_blank">${dado.titulo}</a>
+                        </h2>
+                        <p class="descricao-meta">${dado.descricao}</p>
+                        <a href="${dado.link}" target="_blank">Mais informações</a> 
+                    </div>`
+        }
+       
+    }
+
+    if (!resultados) {
+        resultados  = "<p>Nada foi encontrado</>"
+    }
+    
+    section.innerHTML = resultados
+}
--- dados.js
+++ dados.js
@@ -2,17 +2,20 @@ let dados = [
     {
         titulo:  "Rayssa Leal",
         descricao: "Rayssa Leal é uma skatista brasileira, conhecida como a 'Fada do Skate'. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil, conquistando a prata nos Jogos Olímpicos de Tóquio 2020",
-        link:"https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        link:"https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags:"skate fada sk8 atleta"
     },
     {
         titulo: "Rebeca Andrade",
         descricao: "Rebeca Andrade é uma das maiores ginastas da história do Brasil. Com um talento inegável e uma força de vontade admirável, ela conquistou o coração dos brasileiros e se tornou um ícone do esporte nacional.",
-        link:"https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+        link:"https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+         tags:"judô judo judoka atleta"
     },
     {
         titulo:  'Betriz Souza',
         descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024. Sua vitória histórica colocou o Brasil no mapa do judô mundial.",
-        link:"https://pt.wikipedia.org/wiki/Beatriz_Souza"
+        link:"https://pt.wikipedia.org/wiki/Beatriz_Souza",
+         tags:"ginástica ouro "
     }
    
 ]
